f1a12d75b7d11c0c3dc2df2e277e85f8471a4d94
Robert Carrillo <robertcl795@gmail.com>
2022-12-12 21:58:54 -0600

    feat: add bio, theme toggle animation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import {
   Box,
+  Button,
   Container,
   Heading,
   Image,
@@ -7,6 +9,7 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
+import { BioSection, BioYear } from '../components/bio'
 import Paragraph from '../components/paragraph'
 import Section from '../components/section'
 
@@ -54,8 +57,38 @@ const Page = () => {
         <Paragraph>Robert is a freelance and innovation advocate, with a passion for building digital products that can solve real world problems, using modern designs and toolsets that provide solid and fresh experiences. Currently, working in the product called {''}
         <Link as={NextLink} href="/works/SAUI">Smart Accesibility UI Tool</Link>
         </Paragraph>
-        
+        <Box align="center" my={4}>
+          <NextLink  href="/works">
+            <Button rightIcon={<ChevronRightIcon/>} colorScheme="teal">My Portfolio</Button>
+          </NextLink>
+        </Box>
       </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">Bio</Heading>
+        <BioSection>
+          <BioYear>1995</BioYear>
+          Born in Nayarit, Mexico
+        </BioSection>
+        <BioSection>
+          <BioYear>2017</BioYear>
+          Started working in IBM as part of the student program
+        </BioSection>
+        <BioSection>
+          <BioYear>2018</BioYear>
+          Completed Communications and Electronics Engineering Bachelor program at Universidad de Guadalajara
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Started working in Kyndryl as fullstack developer
+        </BioSection>
+      </Section>
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">I ♥</Heading>
+        <Paragraph>
+          Videogames, Music, Playing piano, Cycling, Movies, Anime, Coding
+        </Paragraph>
+      </Section>
+      
     </Container>
   )
 }
